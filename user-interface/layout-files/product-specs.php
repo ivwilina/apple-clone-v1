a63effc22b3350b6ad7c39e8effcd18836abbcb3
ivwilina <ivwi3103@gmail.com>
2023-09-23 23:33:03 +0700

    test3

--- user-interface/layout-files/product-specs.php
+++ user-interface/layout-files/product-specs.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Apple Store</title>
+    <link rel="stylesheet" href="/baitaplon-final/user-interface/css-files/default-header.css">
+    <link rel="shortcut icon" href="/baitaplon-final/img-files/icons/apple-favicon.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
+    <div class="navigation-bar">
+
+        <?php
+
+        include_once 'default-header.php'
+
+            ?>
+
+    </div>
     
 </body>
+
 </html>
